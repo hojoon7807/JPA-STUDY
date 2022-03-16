1675feaeaa86f97528012f716c3dad99d7b5471c
hojoon <ujuj0202@gmail.com>
2022-03-16 16:42:15 +0900

    ADD: Member Entity로 crud 사용해보기
    jpa는 sql을 추상화한 JPQL이라는 객체지향 쿼리 언어를 제공한다.
    JPQL은 엔티티 객체를 대상으로 쿼리, sql은 데이터베이스 테이블을 기준으로 쿼리를 한다는 것이 가장 큰 차이이다

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,14 +2,73 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager entityManager = emf.createEntityManager();
+        EntityManager em = emf.createEntityManager();
 
-        entityManager.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        /* 등록
+        try{
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("hojoon");
+            em.persist(member);
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        */
+
+        /* 조회
+        try{
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println(findMember.getId());
+            System.out.println(findMember.getName());
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+         */
+
+        /* 수정
+        try{
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("update hojoon");
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+         */
+
+
+        try{
+            //Member findMember = em.find(Member.class, 1L);
+            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(5)
+                    .getResultList();
+            for (Member member : resultList) {
+                System.out.println(member.getName());
+                System.out.println(member.getId());
+            }
+
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
         emf.close();
 
     }
