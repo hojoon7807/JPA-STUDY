5d714be7258aae0c293c0f7119dcaad282a307ab
hojoon <ujuj0202@gmail.com>
2022-03-25 02:48:56 +0900

    ADD: jpql fetch join 연습

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -16,33 +16,59 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Team3 team = new Team3();
-            team.setName("OB");
-            em.persist(team);
+            Team3 teamA = new Team3();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team3 teamB = new Team3();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+            Team3 teamC = new Team3();
+            teamC.setName("팀C");
+            em.persist(teamC);
 
             Member3 member = new Member3();
             member.setAge(20);
             member.setUsername("Hojoon");
-            member.changeTeam(team);
+            member.changeTeam(teamA);
             em.persist(member);
+
+            Member3 member2 = new Member3();
+            member2.setAge(20);
+            member2.setUsername("Hojoon2");
+            member2.changeTeam(teamB);
+            em.persist(member2);
+
+            Member3 member3 = new Member3();
+            member3.setAge(20);
+            member3.setUsername("Hojoon3");
+            member3.changeTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
-            List<Member3DTO> resultList = em.createQuery("select new jpql.domain.Member3DTO(m.age,m.username) from Member3 as m", Member3DTO.class)
+
+//            List<Member3DTO> resultList = em.createQuery("select new jpql.domain.Member3DTO(m.age,m.username) from Member3 as m", Member3DTO.class)
+//                    .getResultList();
+//
+//            List<Member3> resultList1 = em.createQuery("select m from Member3 as m order by m.age desc", Member3.class)
+//                    .setFirstResult(0)
+//                    .setMaxResults(5)
+//                    .getResultList();
+//
+//            List<Member3> resultList2 = em.createQuery("select m from Member3 as m inner join m.team t", Member3.class)
+//                    .getResultList();
+
+            String query = "select m from Member3 m join fetch m.team";
+            List<Member3> members = em.createQuery(query, Member3.class)
                     .getResultList();
 
-            List<Member3> resultList1 = em.createQuery("select m from Member3 as m order by m.age desc", Member3.class)
-                    .setFirstResult(0)
-                    .setMaxResults(5)
-                    .getResultList();
+            for (Member3 member1 : members) {
+                System.out.println("member1.getUsername() = " + member1.getUsername()+","+
+                        "teamName= " + member1.getTeam().getName());
+            }
 
-            List<Member3> resultList2 = em.createQuery("select m from Member3 as m inner join m.team t", Member3.class)
-                    .getResultList();
-
-
-            Member3DTO memberDTO = resultList.get(0);
-            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
-            System.out.println("memberDTO.getUsername() = " + memberDTO.getAge());
 
             tx.commit();
         }catch (Exception e){
